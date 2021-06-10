73a37ec5ad2f848de53564b178617a2c87e67a27
logan.ghilardi <ghilardilogan@gmail.com>
2021-06-10 18:03:39 +0200

    tmp

--- templates/Products/product.php
+++ templates/Products/product.php
@@ -1,29 +1,32 @@
-<?php
-
+<?php 
 require ROOT."/templates/headerView.php";
-
 ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-4 border p-2 m-2">
-                <p>Nom :</p>
-                <p class="p-0"><?=$product->getName()?></p>
+
+<div class="container p-2">
+    <div class="row">
+        <img src="https://fakeimg.pl/286x180/?text=<?php echo $product->getName() ?>" class="col-6" alt="image produit">
+        <div class="col-6">
+            <div class="col-6">
+                <p>Nom du produit : <?php echo $product->getName() ?></p>
             </div>
-            <div class="col-4 border p-2 m-2">
-                <p>Date de naissance :</p>
-                <p class="p-0"><?=$product->getDescription()?></p>
+            <div class="col-6">
+                <p>Prix : <?php echo $product->getPrice() ?>€</p>
             </div>
-            <div class="col-4 border p-2 m-2">
-                <p>Race :</p>
-                <p class="p-0"><?=$product->getPrice()?></p>
+            <div class="col-12">
+                <a href="/spa/public?page=deleteProduct&id=<?php echo $product->getId() ?>" class="btn btn-danger">Supprimer ce produit</a>
+                <a href="/spa/public?page=updateProduct&id=<?php echo $product->getId() ?>" class="btn btn-secondary">Modifier ce produit</a>
             </div>
         </div>
-        <button><a href="/spa/public?page=deleteProduct&id=<?php echo $product->getId() ?>">Delte</a></button>
-        <button><a href="/spa/public?page=updateProduct&id=<?php echo $product->getId() ?>">update</a></button>
+    </div>
+    <hr>
+    <div class="row">
+        <p>Description du produit :</p>
+        <p><?php echo $product->getDescription() ?></p>
+    </div>
+</div>
+        
 
     
-    </div>
-
     
 </body>
 </html>
